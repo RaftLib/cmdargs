bd9a732648132a7d88d205648ab89c97398fe79d
Jonathan Beard <jonathan.c.beard@gmail.com>
2020-06-29 09:41:52 -0500

    updates for pc install

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /scratch/GIT_RPO/cmdargs
+# Build directory: /scratch/GIT_RPO/cmdargs/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib")
+subdirs("include")
+subdirs("testsuite")
